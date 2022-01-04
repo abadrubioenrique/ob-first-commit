c6c14cd40bb329eb1d60670902fff6084e58b367
abadrubioenrique <abadrubioenrique@gmail.com>
2022-01-04 13:49:20 +0100

    modal

--- src/components/modalComponent.jsx
+++ src/components/modalComponent.jsx
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { LEVELS } from '../models/faceToFace-enum';
 import '../styles/select.scss'
 import '../styles/modal.scss';
 import Tagcomponent from './tagComponent';
-const ModalComponent = ({handleClose, show, children}) => {
+const ModalComponent = () => {
 
-/*     const [show, setShow] = useState(false);
+    const [show, setShow] = useState(true);
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true); */
-    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
-    
+    const handleShow = () => setShow(true);
+    useEffect(() => {
+        console.log(show);
+    }, [show])
     return (
 
-    <div className={showHideClassName}>
-    {children}
+    <div className="modal">
+
     <div className="modal-content">
        
-        <span  onClick={handleClose} className="close">&times;</span>
-        <h1 className="modal-title">Nuevo Alumno</h1>
+        <span onClick={handleClose} className="close">&times;</span>
+        <h1 className="modal-title">Nuevo Candidato</h1>
         <div className="modal-container">    
         <div className="a1">
             <label className='credentials'>Nombre y Apellidos</label>
@@ -54,10 +55,14 @@ const ModalComponent = ({handleClose, show, children}) => {
         <Tagcomponent></Tagcomponent>
 
         </div>
+        <div className="f1">
+            <label className='credentials'>Perfil Linkedin</label>
+            <input className='input-text' type='text' placeholder='Enlace a Linkedin'/>
+        </div>
         <div className="Datos container">
             <div className="a">
                 <label className='credentials'>País</label>
-                <select defaultValue="" className='input-text' >
+                <select defaultValue="" className='input-text custom-select' >
                     <option value="" disabled>Elige una opcion</option>  
                     <option>España</option>
                 </select>
@@ -98,6 +103,7 @@ const ModalComponent = ({handleClose, show, children}) => {
                     <option>No</option>
                 </select>
             </div>
+
         </div>
         
 
@@ -112,7 +118,6 @@ const ModalComponent = ({handleClose, show, children}) => {
         
     </div>
     </div>
-    
     </div>
     );
 }
